c0e78f361de074da63e39e105dd7dd17ea181789
Joshua Bergeron <pacluc20@aol.com>
2025-03-26 12:28:28 -0700

    update caching

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,7 +3,8 @@ import { fetchVehicles } from "../lib/data";
 
 export default async function Page() {
   const vehicleList = await fetchVehicles(
-    "410544b2-4001-4271-9855-fec4b6a6442a"
+    "410544b2-4001-4271-9855-fec4b6a6442a",
+    { cache: "no-store" }
   );
 
   return <Garage vehicleList={vehicleList} />;
