325bfb2031429bb4aacde55f09837ce506266116
Joshua Bergeron <pacluc20@aol.com>
2025-03-27 11:30:26 -0700

    force dynamic

--- src/app/api/fetchVehicles/route.js
+++ src/app/api/fetchVehicles/route.js
@@ -1,6 +1,7 @@
 import { fetchVehicles } from "@/app/lib/data";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // add this line
+
 export async function GET(req) {
   try {
     // const user_id = req.nextUrl.searchParams.get("user_id");
